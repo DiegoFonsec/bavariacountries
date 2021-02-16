069ea4faddcff50d33ca4c3ca57889526eb872f5
DiegoFonsec <dafg1505@gmail.com>
2021-02-15 19:44:34 -0500

    g

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -29,7 +29,7 @@ const Filter = ({ onFiltered }) => {
                 <option value="Africa" >Africa</option>
                 <option value="Americas">America</option>
                 <option value="Asia" >Asia</option>
-                <option value="Europe" >Europa</option>
+                <option value="Europe" >Europa </option>
                 <option value="Oceania" >Oceania</option>
             </select>
         </div>
